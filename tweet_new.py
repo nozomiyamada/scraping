b6f07291bda15e6acf7a3de27b7ecaa8dbb1b2f8
nozomiyamada <y.nozomi320@gmail.com>
2020-02-24 16:53:19 +0700

    Update tweet_new.py

--- tweet_new.py
+++ tweet_new.py
@@ -105,8 +105,6 @@ def main():
     button = tk.Button(root, text='START SCRAPING', command=start)
     button.grid(row=14, column=1, columnspan=2)
 
-    fff = tk.StringVar()
-    L = tk.Label(root, textvariable=fff); L.grid(row=15)
     root.mainloop()
 
 ##### functions for scraping #####
@@ -209,7 +207,7 @@ class ScrapeTweet:
         #options = Options()
         #options.add_argument('-headless')
         #driver = webdriver.Firefox(firefox_options=options)
-        driver = webdriver.Chrome()
+        driver = webdriver.Firefox()
         if self.month_from != None:
             self.url += f'since%3A{self.month_from}%20'
         if self.month_to != None:
@@ -237,134 +235,7 @@ class ScrapeTweet:
         else:
             pd.DataFrame(result).to_csv(self.filepath, encoding='utf-8', index=None)
 
-        #driver.close()
-
-    def scrape_tweet_day(self, month, start_date=1):
-        """
-        month: month2013_10 = ['2013-10-1', '2013-10-2',...]
-        """
-        files = sorted(glob.glob(self.path + month[0].rsplit('-',1)[0] + '/*.tsv'))
-        print(sorted([x.rsplit('/')[-1] for x in files]))
-        #options = Options()
-        #options.add_argument('-headless')
-        #driver = webdriver.Firefox(firefox_options=options)
-        driver = webdriver.Firefox()
-
-        for day_idx in tqdm.tqdm(range(start_date-1, len(month)-1), desc='day'):
-            day_since = month[day_idx] 
-            day_until = month[day_idx+1]  # if the time is 23:50, override 'day_to' below
-
-            filename = f'{self.path}{month[0].rsplit("-",1)[0]}/{day_since}.tsv'
-            if filename in files: # if exists, append
-                # open file once for making tweet ID list
-                with open(filename, 'r', encoding='utf-8') as f:
-                    tweet_id_exist = [line[1] for line in csv.reader(f, delimiter='\t')]
-                write_file = open(filename, 'a', encoding='utf-8')
-            else:
-                write_file = open(filename, 'w', encoding='utf-8')
-                tweet_id_exist = []
-            writer = csv.writer(write_file, delimiter='\t', lineterminator='\n')
-
-            url = self.url + f'%20since%3A{day_since}_0:00_ICT%20until%3A{day_until}_0:00_ICT'
-            driver.get(url)
-
-            # scroll k times
-            scrollheight = []
-            for _ in range(self.scroll_time):
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # scroll to the bottom
-                scrollheight.append(driver.execute_script("return document.body.scrollHeight;"))
-                sleep(1)
-                if len(scrollheight) >= 3 and len(set(scrollheight[-3:])) == 1:
-                    break
-
-                # scraping
-                id_compile = re.compile('tweet js-stream-tweet .*')
-                tweet_compile = re.compile('TweetTextSize .*')
-                html = driver.page_source.encode('utf-8')
-                soup = BeautifulSoup(html, "html.parser")  # get html
-                id_html = soup.find_all('div', class_=id_compile)  # get user id and tweet id
-                tweet_html = soup.find_all('p', class_=tweet_compile)  # get tweet and hash tag
-                """ hash tags
-                if tweet_html[k].find('a') is not None:
-                    hashtags = tweet_html[k].find_all('a')
-                    hashtag = [unquote(tag.get('href').split('/hashtag/')[-1].strip('?src=hash')) for tag in hashtags]
-                else:
-                    hashtag = 'None'
-                """
-
-                # check banned tweet
-                id_html_checked = [a for a in id_html if ('because it violates' not in a.text and 'has been withheld' not in a.text and 'This Tweet is unavailable' not in a.text)]
-                for k in range(len(id_html_checked)):
-                    user_id = id_html_checked[k].get('data-permalink-path').split('/status/')[0].strip('/')
-                    tweet_id = id_html_checked[k].get('data-permalink-path').split('/status/')[-1]
-                    tweet = tweet_html[k].text
-                    line = [user_id, tweet_id, tweet]
-                    if tweet_id not in tweet_id_exist:
-                        writer.writerow(line)
-
-            write_file.close()
         driver.close()
 
-    def scrape_from_now(self, filename):
-        """
-        month: month2013_10 = ['2013-10-1', '2013-10-2',...]
-        """
-        #options = Options()
-        #options.add_argument('-headless')
-        #driver = webdriver.Firefox(firefox_options=options)
-        driver = webdriver.Firefox()
-
-        filepath = f'{self.path}/{filename}'
-        if os.path.exists(filepath): # if exists, append
-            # open file once for making tweet ID list
-            with open(filepath, 'r', encoding='utf-8') as f:
-                tweet_id_exist = [line[1] for line in csv.reader(f, delimiter='\t')]
-            write_file = open(filepath, 'a', encoding='utf-8')
-        else:
-            write_file = open(filepath, 'w', encoding='utf-8')
-            tweet_id_exist = []
-        writer = csv.writer(write_file, delimiter='\t', lineterminator='\n')
-
-        url = self.url
-        driver.get(url)
-
-        # scroll k times
-        scrollheight = []
-        for t in range(self.scroll_time):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # scroll to the bottom
-            scrollheight.append(driver.execute_script("return document.body.scrollHeight;"))
-            sleep(1)
-            if len(scrollheight) >= 3 and len(set(scrollheight[-3:])) == 1:
-                break
-
-        # scraping
-        id_compile = re.compile('tweet js-stream-tweet .*')
-        tweet_compile = re.compile('TweetTextSize .*')
-        html = driver.page_source.encode('utf-8')
-        soup = BeautifulSoup(html, "html.parser")  # get html
-        id_html = soup.find_all('div', class_=id_compile)  # get user id and tweet id
-        tweet_html = soup.find_all('p', class_=tweet_compile)  # get tweet and hash tag
-        """ hash tags
-        if tweet_html[k].find('a') is not None:
-            hashtags = tweet_html[k].find_all('a')
-            hashtag = [unquote(tag.get('href').split('/hashtag/')[-1].strip('?src=hash')) for tag in hashtags]
-        else:
-            hashtag = 'None'
-        """
-
-        # check banned tweet
-        id_html_checked = [a for a in id_html if ('because it violates' not in a.text and 'has been withheld' not in a.text and 'This Tweet is unavailable' not in a.text)]
-        for k in range(len(id_html_checked)):
-            user_id = id_html_checked[k].get('data-permalink-path').split('/status/')[0].strip('/')
-            tweet_id = id_html_checked[k].get('data-permalink-path').split('/status/')[-1]
-            tweet = tweet_html[k].text
-            line = [user_id, tweet_id, tweet]
-            if tweet_id not in tweet_id_exist:
-                writer.writerow(line)
-
-        write_file.close()
-        #driver.close()
-"""
 if __name__ == "__main__":
     main()
-"""
